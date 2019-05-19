979443b6fa581f3360e46d1987dcafee288525eb
Kristian Jones <kristianfjonesis@gmail.com>
2019-05-19 16:07:33 -0500

    feat: Styles and looks. Title, favicon

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -54,7 +54,7 @@ export const Layout: FunctionComponent = ({ children }) => {
   useEffect(() => {
     window.addEventListener('resize', () => doSizeCheck());
     doSizeCheck(false);
-  }, [typeof window !== 'undefined'  && window.innerWidth]);
+  }, [typeof window !== 'undefined' && window.innerWidth]);
 
   const doSizeCheck = (initial?: boolean) => {
     const isMobile = window.innerWidth < 640;
@@ -76,7 +76,11 @@ export const Layout: FunctionComponent = ({ children }) => {
       </Toolbar>
       <Head>
         <link rel='manifest' href='/static/manifest.json' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon-16x16.png' />
+        <meta name='theme-color' content='#6200ee' />
         <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet' />
+        <title>IP Addr Network</title>
       </Head>
       <div style={AppStyle}>
         <NavBar open={menuOpen} dismissible={!isMobileState} modal={isMobileState} />
