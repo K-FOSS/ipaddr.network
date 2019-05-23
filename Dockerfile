5d9d1b5ee7c8b3d4b3cf30f6d90871953bb1294e
Kristian Jones <kristianfjonesis@gmail.com>
2019-05-23 15:12:23 -0500

    feat: move server to typescript and build script

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 FROM mhart/alpine-node:latest
 WORKDIR /app
-COPY package.json package-lock.json next.config.js tsconfig.json style.css ./
+COPY package.json package-lock.json tsconfig.json tsconfig.server.json style.css build.ts ./
 COPY ./pages/ ./pages/
 COPY ./static/ ./static/
 COPY ./components ./components/
+COPY ./server ./server 
 RUN npm install && NODE_ENV=production npm run build
+RUN ls
+RUN ls ./server
 
 FROM mhart/alpine-node:latest
 WORKDIR /app
@@ -19,8 +22,9 @@ COPY --from=1 /usr/lib/libgcc* /usr/lib/libstdc* /usr/lib/
 COPY --from=1 /app/node_modules/ /app/node_modules/
 WORKDIR /app
 COPY --from=0 /app/.next/ /app/.next/
+COPY --from=0 /app/dist/ /app/dist/
 COPY static /app/static/
-COPY package.json entrypoint.js /app/
-CMD NODE_ENV=production node /app/entrypoint.js
+COPY package.json /app/
+CMD NODE_ENV=production node /app/dist/
 
 HEALTHCHECK --interval=5s --timeout=5s --retries=3 CMD wget localhost:80 -q -O - > /dev/null 2>&1
---
+++ server/index.ts
@@ -0,0 +1,35 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { parse } from 'url';
+import { join } from 'path';
+import conf from './config';
+const next = require('next') as typeof import('next-server').default;
+
+const app = next({
+  conf: conf
+});
+const handle = app.getRequestHandler();
+
+const StartServer = async () => {
+  const port = 80;
+  await app.prepare();
+  const server = createServer(Handler);
+  server.listen(port, () => console.log(`> Server Started on Port ${port}`));
+};
+
+StartServer();
+
+const Handler = (req: IncomingMessage, res: ServerResponse) => {
+  const parsedUrl = parse(req.url!, true);
+  const { pathname } = parsedUrl;
+
+  // If /service-worker.js load from .next static files
+  if (pathname === '/service-worker.js') {
+    const filePath = join(__dirname, '.next', pathname);
+    app.serveStatic(req, res, filePath);
+  }
+  // Else if legacy /ipcalc then redirect to root
+  else if (pathname === '/ipcalc') {
+    res.writeHead(302, { Location: '/' });
+    res.end();
+  } else handle(req, res, parsedUrl); // All other paths are left to NextJS to deal with
+};
